9dbd1c4e30199e7e5d54241e0dedf09691f94f1b
DrakyNeUwU <89728558+DrakyNeUwU@users.noreply.github.com>
2026-03-08 22:22:26 +0700

    Add files via upload

---
+++ sorts/bubble_sort.h
@@ -0,0 +1,7 @@
+#ifndef bubble_sort.H
+#define bubble_sort .H
+#include <iostream>
+using namespace std;
+void bubbleSort1(int a[], int n);
+void bubbleSort2(int a[], int n, long long &comparision);
+#endif
