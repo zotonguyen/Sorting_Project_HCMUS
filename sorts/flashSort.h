9657dbaa03e3ab89ef9486aadade9119e26fdfeb
sand012 <2512732086@student.hcmus.edu.vn>
2026-03-11 03:26:28 +0700

    Add header file for flashSort and insertionSort

---
+++ sorts/flashSort.h
@@ -0,0 +1,12 @@
+#ifndef flashSort_h
+#define flashSort_h
+
+void flashSort(int a[], int n);
+insertionSort(int a[], int n);
+
+
+void flashSort(int a[], int n, long long &comparison);
+insertionSort(int a[], int n,long long &comparison);
+
+
+#endif
