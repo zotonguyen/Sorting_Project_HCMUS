33ff7c3388d662f3a4ed4653bd6767faead8a4f7
sand012 <2512732086@student.hcmus.edu.vn>
2026-03-11 08:15:42 +0700

    Fix header guard naming in radixSort.h

--- sorts/radixSort.h
+++ sorts/radixSort.h
@@ -1,5 +1,5 @@
-#ifndef radixsort_h
-#define radixsort_h
+#ifndef radixSort_h
+#define radixSort_h
 
 int Max(int a[], int n);
 void countingSort(int a[], int n, int d);
@@ -7,7 +7,7 @@ void radixsort(int a[], int n);
 
 int Max(int a[], int n, long long &comparison);
 void countingSort(int a[], int n, int d, long long &comparison);
-void radixsort(int a[], int n, long long &comparison);
+void radixSort(int a[], int n, long long &comparison);
 
 
 
