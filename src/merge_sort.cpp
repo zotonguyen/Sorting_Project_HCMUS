951ed6ae7b1ec66a1fcb1067b953726384be98d1
thhuule <thule28062007@gmail.com>
2026-03-14 23:23:31 +0700

    Update merge_sort.cpp

--- src/merge_sort.cpp
+++ src/merge_sort.cpp
@@ -1,8 +1,8 @@
 # include <iostream>
 using namespace std;
 #include "merge_sort.h"
+
 void Merge (int left[], int na, int right[], int nb, int a[]) {
-    a [na + nb];
     int i = 0, j = 0, k = 0;
 
     while (i < na && j < nb) {
@@ -44,10 +44,10 @@ void merge_sort(int a[], int n ) {
 }
 
 void Merge (int left[], int na, int right[], int nb, int a[], long long& comparison) {
-    a [na + nb];
     int i = 0, j = 0, k = 0;
 
     while (++comparison && i < na && j < nb) {
+        ++ comparison;
         if (left[i] <= right[j]) {
             a[k++] = left[i++];
         }
@@ -56,27 +56,26 @@ void Merge (int left[], int na, int right[], int nb, int a[], long long& compari
         }
     }
 
-    while (i < na) {
+    while (++ comparison && i < na) {
         a[k++] = left [i++];
     }
 
-    while (j < nb) {
+    while (++ comparison && j < nb) {
         a[k++] = right[j++];
     }
 }
 
 void merge_sort(int a[], int n, long long& comparison ) {
-    comparison = 0;
-    if (n <= 1) return;
+    if (++ comparison && n <= 1) return;
     int mid = n/2;
-    int left [mid];
-    int right [n - mid];
+    int* left = new int [mid];
+    int* right = new int [n - mid];
 
-    for (int i = 0; i < mid; i++) {
+    for (int i = 0; ++comparison && i < mid; i++) {
         left[i] = a[i];
     }
 
-    for (int i = mid; i < n; i++) {
+    for (int i = mid; ++ comparison && i < n; i++) {
         right [i - mid] = a[i];
     }
 
@@ -84,4 +83,7 @@ void merge_sort(int a[], int n, long long& comparison ) {
     merge_sort (right, n - mid, comparison); 
 
     Merge (left, mid, right, n - mid, a, comparison);
+    delete[] left;
+    delete[] right;
 }
+
