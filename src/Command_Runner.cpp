0c7879291a0e0945a893ceca876ce52fab95d8c4
zotonguyen <nmnhat2528@clc.fitus.edu.vn>
2026-03-14 13:51:12 +0700

    Update Command_Runner.cpp

--- src/Command_Runner.cpp
+++ src/Command_Runner.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <chrono>
 using namespace std;
+long long comp;
+double running_time;
 double benchmark(int algorithmIndex, int a[], int n)
 {
     auto start = chrono::high_resolution_clock::now();
@@ -10,51 +12,34 @@ double benchmark(int algorithmIndex, int a[], int n)
     auto end = chrono::high_resolution_clock::now();
     return chrono::duration<double, milli>(end - start).count();
 }
-double benchmark(int algorithmIndex, int a[], int n,long long &comp)
+
+double benchmark(int algorithmIndex, int a[], int n, long long &comp)
 {
     auto start = chrono::high_resolution_clock::now();
-    RunSortComparison(algorithmIndex, a, n,comp);
+    RunSortComparison(algorithmIndex, a, n, comp);
     auto end = chrono::high_resolution_clock::now();
     return chrono::duration<double, milli>(end - start).count();
 }
-static void RunA1(const Command &command)
+void Command_exc(Command command, int a[], int n)
 {
-    cout << "ALGORITHM MODE\n";
-    cout << "Command 1\n";
-    cout << "Algorithm: " << command.algorithm_1 << "\n";
-    cout << "Input file: " << command.inputfile << "\n";
-    cout << "Output parameter: " << command.output_param << "\n";
+    comp = 0;
+    running_time = 0;
+    int OutputIndex = GetOutputParamIndex(command.output_param);
+    int AlgorithmIndex = GetAlgorithmIndex(command.algorithm_1);
+    if (OutputIndex == 0)
+        running_time = benchmark(AlgorithmIndex, a, n);
+    if (OutputIndex == 1)
+        RunSortComparison(AlgorithmIndex, a, n, comp);
+    if (OutputIndex == 2)
+    {
+        running_time = benchmark(AlgorithmIndex, a, n, comp);
+    }
+}
 
-    // TODO:
+void Output_Print(Command command)
+{
     int OutputIndex = GetOutputParamIndex(command.output_param);
 
-    long long comp;
-    double running_time;
-    ifstream fin(command.inputfile);
-    if(!fin.is_open()){
-        cout << "ko mo dc ";
-        return ;
-    }
-    int n;
-    fin >> n;
-    int *a = new int[n];
-    for (int i = 0; i < n; i++)
-        fin >> a[i];
-    
-    int x = GetAlgorithmIndex(command.algorithm_1);
-    if(OutputIndex == 0) running_time = benchmark(x, a, n);
-    if(OutputIndex == 1)     RunSortComparison(x, a, n, comp);
-    if(OutputIndex == 2 ) {
-        running_time = benchmark(x,a,n,comp);
-    }
-    ofstream fout("output.txt");
-    cout << "----------" << endl;
-
-    for (int i = 0; i < n; i++)
-        fout << a[i] << ' ';
-    delete[] a;
-    fout.close();
-    fin.close();
     switch (OutputIndex)
     {
     case 0:
@@ -69,23 +54,72 @@ static void RunA1(const Command &command)
     default:
         break;
     }
+}
+
+static void RunA1(const Command &command)
+{
+
+    cout << "ALGORITHM MODE\n";
+    cout << "Command 1\n";
+    cout << "Algorithm: " << command.algorithm_1 << "\n";
+    cout << "Input file: " << command.inputfile << "\n";
+    cout << "Output parameter: " << command.output_param << "\n";
+    cout << "--------------------------------------------------------" << endl;
+
+    // TODO:
+    int OutputIndex = GetOutputParamIndex(command.output_param);
+    ifstream fin(command.inputfile);
+    if (!fin.is_open())
+    {
+        cout << "ko mo dc ";
+        return;
+    }
+    int n;
+    fin >> n;
+    int *a = new int[n];
+    for (int i = 0; i < n; i++)
+        fin >> a[i];
+
+    Command_exc(command, a, n);
+    ofstream fout("output.txt");
+
+    for (int i = 0; i < n; i++)
+        fout << a[i] << ' ';
+    delete[] a;
+    fout.close();
+    fin.close();
+    Output_Print(command);
+
     cout << endl
          << endl;
 }
+
 static void RunA2(const Command &command)
 {
+
     cout << "ALGORITHM MODE\n";
     cout << "Command 2\n";
     cout << "Algorithm: " << command.algorithm_1 << "\n";
     cout << "Input size: " << command.inputsize << "\n";
     cout << "Input order: " << command.inputOrder << "\n";
     cout << "Output parameter: " << command.output_param << "\n";
-
-    // TODO:
-    // Generate data
-    // Write input.txt
-    // Call sort
-    // Write output.txt
+    cout << "--------------------------------------------------------" << endl;
+    int n = command.inputsize;
+    int *a = new int[n];
+    int DataGen = GetGenerateData(command.inputOrder);
+    ofstream fin("input.txt");
+    ofstream fout("output.txt");
+    GenerateData(a, n, DataGen);
+    fin << n << endl;
+    for (int i = 0; i < n; i++)
+        fin << a[i] << ' ';
+    Command_exc(command, a, n);
+    for (int i = 0; i < n; i++)
+        fout << a[i] << ' ';
+    delete[] a;
+    Output_Print(command);
+    fin.close();
+    fout.close();
 }
 
 static void RunA3(const Command &command)
@@ -95,10 +129,60 @@ static void RunA3(const Command &command)
     cout << "Algorithm: " << command.algorithm_1 << "\n";
     cout << "Input size: " << command.inputsize << "\n";
     cout << "Output parameter: " << command.output_param << "\n";
+    cout << "--------------------------------------------------------" << endl;
 
-    // TODO:
-    // Generate 4 cases
-    // Write input_1.txt, input_2.txt, input_3.txt, input_4.txt
+    int n = command.inputsize;
+    int *a = new int[n];
+    cout << "Randomize" << endl;
+    // Random
+    ofstream fin("input_1.txt");
+    fin << n << endl;
+    GenerateData(a, n, 0);
+    for (int i = 0; i < n; i++)
+        fin << a[i] << " ";
+    Command_exc(command, a, n);
+    Output_Print(command);
+    cout << endl;
+    fin.close();
+    // Nearly Sorted
+    cout << "--------------------------------------------------------" << endl;
+    cout << "Nearly Sorted" << endl;
+    ofstream fin1("input_2.txt");
+    fin1 << n << endl;
+    GenerateData(a, n, 3);
+    for (int i = 0; i < n; i++)
+        fin1 << a[i] << " ";
+    Command_exc(command, a, n);
+    Output_Print(command);
+    cout << endl;
+    fin1.close();
+    cout << "--------------------------------------------------------" << endl;
+
+    // Sorted
+    cout << "Sorted" << endl;
+    ofstream fin2("input_3.txt");
+    fin2 << n << endl;
+    GenerateData(a, n, 1);
+    for (int i = 0; i < n; i++)
+        fin2 << a[i] << " ";
+    Command_exc(command, a, n);
+    Output_Print(command);
+    cout << endl;
+    fin2.close();
+    cout << "--------------------------------------------------------" << endl;
+
+    // Reverse Data
+    cout << "Reverse" << endl;
+    ofstream fin3("input_4.txt");
+    fin3 << n << endl;
+    GenerateData(a, n, 2);
+    for (int i = 0; i < n; i++)
+        fin3 << a[i] << " ";
+    Command_exc(command, a, n);
+    Output_Print(command);
+    cout << endl;
+    fin3.close();
+    delete[] a;
 }
 
 static void RunC4(const Command &command)
@@ -108,11 +192,33 @@ static void RunC4(const Command &command)
     cout << "Algorithm 1: " << command.algorithm_1 << "\n";
     cout << "Algorithm 2: " << command.algorithm_2 << "\n";
     cout << "Input file: " << command.inputfile << "\n";
+    cout << "--------------------------------------------------------" << endl;
+    double running_time2 = 0;
+    long long comp2 = 0;
+    int alg1 = GetAlgorithmIndex(command.algorithm_1);
+    int alg2 = GetAlgorithmIndex(command.algorithm_2);
+    ifstream fin(command.inputfile);
+    if (!fin.is_open())
+        cout << "ko mo dc";
+    int n;
+    fin >> n;
+    int *a = new int[n];
+    int *b = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        fin >> a[i];
+    }
+    for (int i = 0; i < n; i++)
+        b[i] = a[i];
+    running_time = benchmark(alg1, a, n, comp);
+    running_time2 = benchmark(alg2, b, n, comp2);
+    int OutputIndex = GetOutputParamIndex(command.output_param);
+    cout << "Running Time: " << running_time << "ms" << " | " << running_time2 << "ms" << '\n';
+    cout << "Comparisions: " << comp << " | " << comp2;
 
-    // TODO:
-    // Read file
-    // Copy array
-    // Compare 2 algorithms
+    fin.close();
+    delete[] a;
+    delete[] b;
 }
 
 static void RunC5(const Command &command)
@@ -123,7 +229,28 @@ static void RunC5(const Command &command)
     cout << "Algorithm 2: " << command.algorithm_2 << "\n";
     cout << "Input size: " << command.inputsize << "\n";
     cout << "Input order: " << command.inputOrder << "\n";
-
+    cout << "--------------------------------------------------------" << endl;
+    int n = command.inputsize;
+    double running_time2=0;
+    long long comp2;
+    int *a = new int[n];
+    int *b = new int[n];
+    int Datatype = GetGenerateData(command.inputOrder);
+    GenerateData(a, n, Datatype);
+    for (int i = 0; i < n; i++)
+        b[i] = a[i];
+    ofstream fout("output.txt");
+    for (int i = 0; i < n; i++)
+        fout << a[i] << ' ';
+    fout.close();
+    int alg1 = GetAlgorithmIndex(command.algorithm_1);
+    int alg2 = GetAlgorithmIndex(command.algorithm_2);
+     running_time = benchmark(alg1, a, n, comp);
+    running_time2 = benchmark(alg2, b, n, comp2);
+      cout << "Running Time: " << running_time << "ms" << " | " << running_time2 << "ms" << '\n';
+    cout << "Comparisions: " << comp << " | " << comp2;
+       delete[] a;
+    delete[] b;
     // TODO:
     // Generate data
     // Write input.txt
