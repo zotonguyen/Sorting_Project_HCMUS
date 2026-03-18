750c8b4fb617a71c4222165a38bbfdb75591f344
zotonguyen <nmnhat2528@clc.fitus.edu.vn>
2026-03-18 17:25:02 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(int argc,char *argv[]){
     Command cmd;
     if(!Handle_command(argc,argv,cmd)){
-        cout << "No cmt";
+        cout << "Unknow Command";
     }
     RunCommand(cmd);
     
